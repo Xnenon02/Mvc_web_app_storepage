2fbeea72fb1e5a9da11ca1945eea96386ff658ed
tomekstrand78@gmail.com <tomekstrand78@gmail.com@XNENON02>
2026-03-18 14:38:19 +0100

    Initial commit for new MVC shop project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Desktop()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
