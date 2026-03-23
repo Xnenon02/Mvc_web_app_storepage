94a6c62cd2e060b44232a1f9f172c0b5217aec56
Xnenon02 <tomekstrand78@gmail.com>
2026-03-23 14:20:30 +0100

    updated code structure and clean up code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,10 +15,6 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult Desktop()
-    {
-        return View();
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMvcApp.Data;
+using MyMvcApp.Models;
+
+namespace MyMvcApp.Controllers;
+
+public class ProductsController : Controller
+{
+    public IActionResult Desktop()
+    {
+        var desktops = ProductData.Products
+            .Where(p => p.Category == "Desktop")
+            .ToList();
+
+        return View(desktops);
+    }
+
+    public IActionResult Laptops()
+    {
+        var laptops = ProductData.Products
+            .Where(p => p.Category == "Laptop")
+            .ToList();
+
+        return View(laptops);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var product = ProductData.Products
+            .FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+}
